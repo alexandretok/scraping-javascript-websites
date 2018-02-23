782c22d80d5034b26c5257f9c588f3de6dd9b1cb
Alexandre Justino <alexandretok@gmail.com>
2018-02-23 01:16:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,18 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const cheerio = require('cheerio')
 
-var url = "https://www.americanas.com.br/categoria/alimentos-e-bebidas/vinhos?ordenacao=bestSellers&origem=omega";
+var url = "https://rawgit.com/alexandretok/scraping-javascript-websites/master/dummy.html";
 
-var req = {uri:url};
-
-request(req,function (error, response, page) {
+request({uri:url},function (error, response, page) {
     if (!error && response.statusCode == 200) {
         JSDOM.fromURL("https://www.americanas.com.br/categoria/alimentos-e-bebidas/vinhos?ordenacao=bestSellers&origem=omega", {runScripts: "dangerously", pretendToBeVisual: true, resources: "usable"}).then(dom => {
-            console.log("Pagina recebida.");
-
-            // Timeout para dar tempo do JavaScript executar
+            console.log("DOM Created. Waiting a while for JavaScript code to run...");
             setTimeout(() => {
-                console.log("Criando DOM");
-
+                // Parsing DOM with cheerio for easier extraction
                 const $ = cheerio.load(dom.serialize());
 
-                $('body .szr-39 .sz .mmn-itm-link').each(function(index, item){
-                    console.log($(this).text().replace(/^(\s+)|(\s+)$/g, '').replace(/(\s{2,})/g, ' ').replace(' Novo', ''));
-                });                
-            }, 5000);
+                console.log($('li').text());
+            }, 1000);
         });
     }
 });
